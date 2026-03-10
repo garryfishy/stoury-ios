2f05a3095e8e7623e77bcf2f17a2ee291ef4a40f
Garry Agassi <agassi.garry@gmail.com>
2026-03-10 12:05:35 +0700

    Initial Commit

---
+++ stoury/stouryApp.swift
@@ -0,0 +1,17 @@
+//
+//  stouryApp.swift
+//  stoury
+//
+//  Created by Garry Agassi on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct stouryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
